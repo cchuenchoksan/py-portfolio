57b6ffb569b07ed2427f4c70d0dfd2cb84042f48
cchuenchoksan <cchuenchoksan@gmail.com>
2024-10-16 23:22:39 +0800

    Add cml port

--- pyportfolio.py
+++ pyportfolio.py
@@ -7,19 +7,71 @@ import requests
 from bs4 import BeautifulSoup
 
 
-class PyPortfolio:
-    def __init__(self, tickers: list, rf=None, start="2014-01-01", end="2023-12-31"):
+class YFData:
+    @staticmethod
+    def get_sg_risk_free_rate():
+        """Function to get the risk free rate from the singapore government website"""
+        url = 'https://eservices.mas.gov.sg/statistics/fdanet/SgsBenchmarkIssuePrices.aspx'
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            print("Failed to set rf", response.status_code)
+            print("Setting rf to 0.03")
+            return 0.03
+
+        soup = BeautifulSoup(response.content)
+        table_html = str(soup.find_all("table")[0])
+
+        df_rf = pd.read_html(table_html)[0]
+        df_rf.head(10)
+        return np.mean(df_rf["Treasury Bills"].iloc[-1].to_numpy()) / 100
+
+
+    @staticmethod
+    def _get_info(tickers: list):
+        """Function that returns the stock information from the tickers"""
+        info_list = []
+        for ticker in tickers:
+            stock = yf.Ticker(ticker)
+            info = stock.info
+            market_cap = info.get('marketCap', 'N/A')
+
+            info_list.append({
+                'Ticker': ticker,
+                'Company': info.get('longName', info.get('name', 'Unknown')),
+                'Sector': info.get('sector', 'N/A'),
+                'Market Cap (B SGD)': market_cap / 1e9 if market_cap != 'N/A' else 'N/A'
+            })
+
+        data = pd.DataFrame(info_list)
+        return data
+
+
+    @staticmethod
+    def get_returns(stock_returns, weights):
+        return np.dot(weights, stock_returns)
+
+
+    @staticmethod
+    def get_std(stock_cov, weights):
+        return np.sqrt(np.dot(weights.T, np.dot(stock_cov, weights)))
+
+
+class PyPortfolio(YFData):
+    def __init__(self, tickers: list, rf=None, start="2014-01-01", end="2023-12-31", risk_profile=0.9):
         if rf is None:
             self.rf = self.get_sg_risk_free_rate()
         else:
             self.rf = rf
         
+        self.risk_profile = risk_profile
         self.update_tickers(tickers, start, end)
 
         # Private for optimisation
         self._eq_constraint = {'type': 'eq', 'fun': self._equality_constraint}
         self._initial_weights = np.array([1/len(tickers) for _ in range(len(tickers))])
         self._bounds = [(-10, 10) for _ in range(len(self._initial_weights))]
+
 
     def update_tickers(self, tickers, start="2014-01-01", end="2023-12-31"):
         """Method to run when you would like to update the stocks in the portfolio"""
@@ -43,13 +95,13 @@ class PyPortfolio:
 
     def _objective_min_vol(self):
         """Returns an objective function which gives the volatility of the portfolio"""
-        return lambda weights: np.sqrt(np.dot(weights.T, np.dot(self.cov_matrix, weights))) * 100
+        return lambda weights: self.get_std(self.cov_matrix, weights) * 100
 
 
     def _objective_max_sharpe(self):
         """Returns an objective function which gives the negative sharpe ratio"""
-        return lambda weights: -((np.dot(weights, self.expected_returns) - self.rf) 
-                                / np.sqrt(np.dot(weights.T, np.dot(self.cov_matrix, weights))))
+        return lambda weights: -((self.get_returns(self.expected_returns, weights) - self.rf) 
+                                /  self.get_std(self.cov_matrix, weights))
 
     
     @staticmethod
@@ -134,8 +186,8 @@ class PyPortfolio:
             weights = self._generate_numbers(n=len(self.tickers))
             weights_record.append(weights)
 
-            portfolio_return = np.dot(weights, self.expected_returns)
-            portfolio_stddev = np.sqrt(np.dot(weights.T, np.dot(self.cov_matrix, weights)))
+            portfolio_return = self.get_returns(self.expected_returns, weights)
+            portfolio_stddev = self.get_std(self.cov_matrix, weights)
             sharpe_ratio = (portfolio_return - self.rf) / portfolio_stddev
 
             results[0,i] = portfolio_return
@@ -145,41 +197,34 @@ class PyPortfolio:
         return results, weights_record
 
 
-    @staticmethod
-    def get_sg_risk_free_rate():
-        """Function to get the risk free rate from the singapore government website"""
-        url = 'https://eservices.mas.gov.sg/statistics/fdanet/SgsBenchmarkIssuePrices.aspx'
-        response = requests.get(url)
-        
-        if response.status_code != 200:
-            print("Failed to set rf", response.status_code)
-            print("Setting rf to 0.03")
-            return 0.03
-
-        soup = BeautifulSoup(response.content)
-        table_html = str(soup.find_all("table")[0])
-
-        df_rf = pd.read_html(table_html)[0]
-        df_rf.head(10)
-        return np.mean(df_rf["Treasury Bills"].iloc[-1].to_numpy()) / 100
+    def get_risk_free_port(self, weights=None):
+        """This function gives the portfolio depending on the optimal portfolio and rf on the capital market line."""
+        if weights is None:
+            weights, _, _  = self.get_optimise_port()
+        weights = np.array(weights) * self.risk_profile
+        weights = np.append(weights, [1 - self.risk_profile])
+        risk_free_df_r = self.daily_r.copy()
+        risk_free_df_r["__rf"] = self.rf / 252
+        expected_returns = risk_free_df_r.mean() * 252  # assume 252 trading days
+        cov_matrix = risk_free_df_r.cov() * 252
+        return weights, self.get_returns(expected_returns, weights), self.get_std(cov_matrix, weights)
 
 
-    @staticmethod
-    def _get_info(tickers: list):
-        """Function that returns the stock information from the tickers"""
-        info_list = []
-        for ticker in tickers:
-            stock = yf.Ticker(ticker)
-            info = stock.info
-            market_cap = info.get('marketCap', 'N/A')
+if __name__ == "__main__":
+    stock_tickers = [  # Some random tickers to demonstrate the class usage
+        "D05.SI",
+        "O39.SI",
+        "Z74.SI",
+        "G07.SI",
+        "C07.SI",
+        "S58.SI",
+        "C09.SI",
+        # "K71U.SI",
+        # "HMN.SI",
+        # "E5H.SI",
+        # "NS8U.SI",
+    ]
 
-            info_list.append({
-                'Ticker': ticker,
-                'Company': info.get('longName', info.get('name', 'Unknown')),
-                'Sector': info.get('sector', 'N/A'),
-                'Market Cap (B SGD)': market_cap / 1e9 if market_cap != 'N/A' else 'N/A'
-            })
-
-        data = pd.DataFrame(info_list)
-        return data
-
+    portfolio = PyPortfolio(stock_tickers)
+    weights, _, _ =portfolio.get_risk_free_port()
+    print(sum(weights))
